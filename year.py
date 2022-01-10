b0b6c7a8fc2fe2e054325f188c94779f0902df3c
Grace-Hephzibah <mgracehephzibah@gmail.com>
2022-01-10 18:16:01 +0530

    Git Commit

---
+++ year.py
@@ -0,0 +1,28 @@
+import January as m1
+import February as m2
+import March as m3
+import April as m4
+import May as m5
+import June as m6
+import July as m7
+import August as m8
+import September as m9
+import October as m10
+import November as m11
+import December as m12
+
+year = [
+    m1.January,
+    m2.February,
+    m3.March,
+    m4.April,
+    m5.May,
+    m6.June,
+    m7.July,
+    m8.August,
+    m9.September,
+    m10.October,
+    m11.November,
+    m12.December,
+]
+
